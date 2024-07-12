9e974290e9c352df35e221124b4d4ae275793139
Akos Szabo-Gali <szabogaliakos@gmail.com>
2024-07-12 19:13:10 +0200

    Loader added

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Label() {
+  return (
+    <div className="Callback-loader">
+      <div className="Loader"></div>
+      <p></p>
+    </div>
+  );
+}
